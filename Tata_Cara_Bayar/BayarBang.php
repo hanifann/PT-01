a99e8f5692f3eebba1a3b5bce18b237279ebec1b
hanifann <hanifannurulhaq@outlook.com>
2019-03-25 00:48:46 +0700

    home

--- Tata_Cara_Bayar/BayarBang.php
+++ Tata_Cara_Bayar/BayarBang.php
@@ -92,7 +92,7 @@ if(isset($_COOKIE["login"])){
     <!--end of navbar 1-->
     <!--navbar 2-->
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#3FC0B7;">
-    <a class="navbar-brand" href="main.php">Home</a>
+    <a class="navbar-brand" href="../main.php">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
