74f1f2dc1bfe5f8fde709b64607a4dc1a9f47518
hanifann <hanifannurulhaq@outlook.com>
2019-03-24 23:30:20 +0700

    ajaxnya bisa bossque

--- Tata_Cara_Bayar/BayarBang.php
+++ Tata_Cara_Bayar/BayarBang.php
@@ -18,13 +18,25 @@ if(isset($_COOKIE["login"])){
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
   integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
   crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <meta charset="utf-8">
     <link rel="stylesheet" href="BayarBang.css">
+    <script>
+      $(document).ready(function(){
+        $("#bni").click(function(){
+          $("#karepmu").load("bca.php");
+        });
+      });
+      $(document).ready(function(){
+        $("#bca").click(function(){
+          $("#karepmu").load("bca.php");
+        });
+      });
+      </script>
   </head>
 
   <body>
@@ -88,10 +100,10 @@ if(isset($_COOKIE["login"])){
   <!--end of navbar 2-->
 
 <!-- PANEL -->
-<div class="container border mt-3">
+<div id="karepmu" class="container border mt-3">
   <h5 class="text-center mt-4">Total Pembelian</h5>
   <h5 class="text-center" style="color:#FF7300;">Rp. 500.000</h5>
-  <table class="table table-hover">
+  <table id="tabelBayar" class="table table-hover">
     <tbody>
       <tr>
         <td>
@@ -100,10 +112,12 @@ if(isset($_COOKIE["login"])){
               <img style="width:50%;" src="bni.png" alt="">
             </div>
             <div class="col mt-1">
-              <h6>BNI</h6>
+              <h7>BNI</h7>
             </div>
             <div class="col-lg-1">
-              <img src="arrow.png" alt="">
+              <button style="border:none;background:none;" id="bni" name="button">
+                <img src="arrow.png" alt="">
+              </button>
             </div>
           </div>
         </td>
@@ -115,10 +129,12 @@ if(isset($_COOKIE["login"])){
               <img style="width:50%;" src="bri.png" alt="">
             </div>
             <div class="col mt-1">
-              <h6>BRI</h6>
+              <h7>BRI</h7>
             </div>
             <div class="col-lg-1">
-              <img src="arrow.png" alt="">
+              <button style="border:none;background:none;" id="bri" name="button">
+                <img src="arrow.png" alt="">
+              </button>
             </div>
           </div>
         </td>
@@ -130,10 +146,12 @@ if(isset($_COOKIE["login"])){
               <img style="width: 61.5px" src="bca.png" class="img-fluid" alt="">
             </div>
             <div class="col mt-1 ml-3">
-              <h6 class="ml-5">BCA</h6>
+              <h7 class="ml-5">BCA</h7>
             </div>
             <div class="col-lg-1">
-              <img src="arrow.png" alt="">
+              <button style="border:none;background:none;" id="bca" name="button">
+                <img src="arrow.png" alt="">
+              </button>
             </div>
           </div>
         </td>
@@ -145,12 +163,49 @@ if(isset($_COOKIE["login"])){
               <img style="width:50%;" src="mandiri.png" alt="">
             </div>
             <div class="col mt-1">
-              <h6>MANDIRI</h6>
+              <h7>Mandiri</h7>
             </div>
             <div class="col-lg-1">
-              <img src="arrow.png" alt="">
+              <button style="border:none;background:none;" id="mandiri" name="button">
+                <img src="arrow.png" alt="">
+              </button>
             </div>
           </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="row">
+            <div class="col-md-auto">
+              <img style="width: 61.5px" src="hana.png" class="img-fluid" alt="">
+            </div>
+            <div class="col mt-1 ml-3">
+              <h7 class="ml-5">Hana bank</h7>
+            </div>
+            <div class="col-lg-1">
+              <button style="border:none;background:none;" id="hana" name="button">
+                <img src="arrow.png" alt="">
+              </button>
+            </div>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div class="row">
+            <div class="col-md-auto">
+              <img style="width: 61.5px" src="maybank.png" class="img-fluid" alt="">
+            </div>
+            <div class="col mt-1 ml-3">
+              <h7 class="ml-5">Maybank</h7>
+            </div>
+            <div class="col-lg-1">
+              <button style="border:none;background:none;" type="maybank" name="button">
+                <img src="arrow.png" alt="">
+              </button>
+            </div>
+          </div>
+          <hr>
         </td>
       </tr>
     </tbody>
