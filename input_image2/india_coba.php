2a490d20797daa90e3462ac43cfd8d708c9cb55f
hasbifm <hasbifhasbi@gmail.com>
2019-03-24 14:11:44 +0700

    mantul bangsat

---
+++ input_image2/india_coba.php
@@ -0,0 +1,57 @@
+<?php
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <form class="" enctype="multipart/form-data" method="post">
+
+       <input type="file" name="image" value="">
+       <input type="submit" name="submit" value="upload">
+
+     </form>
+
+     <?php
+
+     if (isset($_POST['submit'])) {
+       if (getimagesize($_FILES['image']['tmp_name']) == false) {
+         echo "image bang";
+       }else {
+         $image = addslashes($_FILES['image']['tmp_name']);
+         $name = addslashes($_FILES['image']['name']);
+         $image = file_get_contents($image);
+         $image = base64_encode($image);
+         saveimages($name,$image);
+
+       // code...
+     }
+   }
+   tampil();
+     function saveimages($name,$image){
+       $con = mysqli_connect("localhost","root","","coba_gambar");
+       $query = "INSERT INTO images(name,image) VALUES ('$name','$image')";
+       $result = mysqli_query($con,$query);
+       if($result){
+         echo "<br/>Mantul bang";
+       }else{
+         echo "<br/>gagal upload bang.";
+       }
+     }
+     function tampil(){
+       $con = mysqli_connect("localhost","root","","coba_gambar");
+       $query = "SELECT * FROM images";
+       $result = mysqli_query($con,$query);
+
+       while ($row = mysqli_fetch_array($result)){
+         echo '<img src="data:image;base64,'.$row[2].'">';
+       }
+     }
+     ?>
+   </body>
+ </html>
